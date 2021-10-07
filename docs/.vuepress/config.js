24acc3e92244600fc072b8690ccab486fb5cdd71
YeungWanLum <295386487@qq.com>
2021-10-07 10:13:45 +0800

    update: 更新Node文档

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,83 @@
+module.exports = {
+  base: '/NodeCourse/',
+  title: "Node技术文档",
+  description: "Node分享的相关及文档",
+  head: [
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: `/images/icons/favicon-16x16.png`,
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: `/images/icons/favicon-32x32.png`,
+      },
+    ],
+    [
+      'meta',
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
+    ],
+    [
+      'link',
+      { rel: 'apple-touch-icon', href: `/images/icons/apple-touch-icon.png` },
+    ],
+    [
+      'link',
+      {
+        rel: 'mask-icon',
+        href: '/images/icons/safari-pinned-tab.svg',
+        color: '#3eaf7c',
+      },
+    ],
+    ['meta', { name: 'msapplication-TileColor', content: '#3eaf7c' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+  ],
+  markdown: {
+    code: {
+      lineNumbers: 4
+    }
+  },
+
+  theme: '@vuepress/default',
+  themeConfig: {
+    logo: 'https://avatars.githubusercontent.com/u/57422303?s=200&v=4',
+    docsDir: 'docs',
+    sidebar: {
+      collapsable: false,
+      '/guide/': require('./sidebar')
+    },
+ 
+    // page meta
+    editLinkText: '在 GitHub 上编辑此页',
+    lastUpdatedText: '上次更新',
+    contributorsText: '贡献者',
+
+    // custom containers
+    tip: '提示',
+    warning: '注意',
+    danger: '警告',
+
+
+    // 404 page
+    notFound: [
+      '这里什么都没有',
+      '我们怎么到这来了？',
+      '这是一个 404 页面',
+      '看起来我们进入了错误的链接',
+    ],
+    backToHome: '返回首页',
+
+    // a11y
+    openInNewWindow: '在新窗口打开',
+    toggleDarkMode: '切换夜间模式',
+    toggleSidebar: '切换侧边栏',
+  }
+};
---
+++ docs/.vuepress/sidebar.js
@@ -0,0 +1,14 @@
+const sidebar = [
+  {
+    text: '开篇',
+    collapsable: false,
+    children: [
+      '/guide/01-daolun.md',
+      '/guide/02-lishi.md',
+      '/guide/03-jibenyufa.md',
+    ]
+  }
+];
+
+
+module.exports = sidebar;
