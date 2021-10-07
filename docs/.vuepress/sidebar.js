7216637991ebb4af3f74ece853d6c4b1ee53d5f6
YeungWanLum <295386487@qq.com>
2021-10-07 13:00:47 +0800

    :tada: node文档开始

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -3,9 +3,7 @@ const sidebar = [
     text: '开篇',
     collapsable: false,
     children: [
-      '/guide/01-daolun.md',
-      '/guide/02-lishi.md',
-      '/guide/03-jibenyufa.md',
+      '/guide/01-开篇.md'
     ]
   }
 ];
