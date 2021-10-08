ea1f2cd2bd84c117134b6cb0c0f720fb22cbeb2a
YeungWanLum <295386487@qq.com>
2021-10-09 07:21:33 +0800

    update: Koa文档

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -3,7 +3,12 @@ const sidebar = [
     text: 'Koa框架',
     collapsable: false,
     children: [
-      '/guide/01-简介.md'
+      '/guide/01-简介.md',
+      '/guide/02-路由.md',
+      '/guide/03-请求数据获取.md',
+      '/guide/04-静态资源加载.md',
+      '/guide/05-跨域中间件.md',
+      '/guide/06-数据库操作.md'
     ]
   }
 ];
